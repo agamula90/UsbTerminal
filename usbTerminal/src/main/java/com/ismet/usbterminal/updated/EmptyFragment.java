bf640d4326792ec1941aebff8a55cbeb50b8e1b5
pm_237 <hX4hIjytWG>
2016-01-08 13:57:42 +0200

    [CHANGE] made copy using instead of originals

---
+++ usbTerminal/src/main/java/com/ismet/usbterminal/updated/EmptyFragment.java
@@ -0,0 +1,21 @@
+package com.ismet.usbterminal.updated;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EmptyFragment extends Fragment {
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+	Bundle savedInstanceState) {
+		View view = new View(container.getContext());
+		view.setBackgroundColor(Color.TRANSPARENT);
+		return view;
+	}
+}
